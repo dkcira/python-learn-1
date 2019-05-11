d24e8ef4f531dca09065288b37d9b8bc5ea98ef6
Dorian Kcira <dkcira@gmail.com>
2019-05-11 12:00:49 -0700

    fix for python3

--- anti_vowel.py
+++ anti_vowel.py
@@ -6,5 +6,11 @@ def anti_vowel(text):
         c = text[i]
         if not (c in the_vowel or c.lower() in the_vowel):
             newstring += c
-    print newstring
+    print(newstring)
     return newstring
+
+if __name__ == '__main__':
+    test_text = "test text for anti vowel"
+    print('original:', test_text)
+    print('anti vowel:')
+    anti_vowel(test_text)
--- battleship.py
+++ battleship.py
@@ -7,9 +7,9 @@ for x in range(5):
 
 def print_board(board):
     for row in board:
-        print " ".join(row)
+        print(" ".join(row))
 
-print "Let's play Battleship!"
+print("Let's play Battleship!")
 print_board(board)
 
 def random_row(board):
@@ -20,28 +20,28 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
+print(ship_row)
+print(ship_col)
 
 # Everything from here on should go in your for loop!
 # Be sure to indent four spaces!
 for turn in range(4):
-    print "Turn", turn + 1
-    guess_row = int(raw_input("Guess Row:"))
-    guess_col = int(raw_input("Guess Col:"))
+    print("Turn", turn + 1)
+    guess_row = int(input("Guess Row:"))
+    guess_col = int(input("Guess Col:"))
     
     if guess_row == ship_row and guess_col == ship_col:
-        print "Congratulations! You sunk my battleship!"
+        print("Congratulations! You sunk my battleship!")
     else:
         if (guess_row < 0 or guess_row > 5) or (guess_col < 0 or guess_col > 5):
-            print "Oops, that's not even in the ocean."
+            print("Oops, that's not even in the ocean.")
         elif(board[guess_row-1][guess_col-1] == "X"):
-            print "You guessed that one already."
+            print("You guessed that one already.")
         else:
-            print "You missed my battleship!"
+            print("You missed my battleship!")
             board[guess_row-1][guess_col-1] = "X"
         # Print (turn + 1) here!
         print_board(board)
     if turn == 3:
-        print "Game Over"
+        print("Game Over")
 
