ed9551242b5dfbe57ad71fb7b1ad28901081e9fe
Dorian Kcira <dkcira@gmail.com>
2019-05-12 20:40:55 -0700

    python3

--- printwithcomma.py
+++ printwithcomma.py
@@ -3,11 +3,11 @@ phrase = "A bird in the hand..."
 # Add your for loop
 for char in phrase:
     if char == 'A' or char == 'a':
-        print 'X',
+        print('X', end=' ')
     else:
-        print char,
+        print(char, end=' ')
 
 
 
 #Don't delete this print statement!
-print
+print()
