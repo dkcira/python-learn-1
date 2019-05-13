d1fd00ac9c35ae95adb15601e928a2daaab1d3ac
Dorian Kcira <dkcira@gmail.com>
2019-05-12 20:48:18 -0700

    python3

--- purify.py
+++ purify.py
@@ -4,5 +4,9 @@ def purify(seq):
     for i in seq:
         if i%2 == 0:
             new_seq.append(i)
-    print new_seq
+    print(new_seq)
     return new_seq
+
+if __name__ == "__main__":
+    seq = [1,2,3,4,5,6,7,8]
+    print(f'sequence: {seq}, purified: {purify(seq)}')
