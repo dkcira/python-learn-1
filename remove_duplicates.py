e7df24c1136106388910b4411b6bf6af7aa27f6b
Dorian Kcira <dkcira@gmail.com>
2019-05-12 20:50:39 -0700

    add description with dates

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -5,3 +5,7 @@ def remove_duplicates(myls):
         if  not (i in newls):
             newls.append(i)
     return newls
+
+if __name__ == "__main__":
+    myls = [1, 2, 3, 2, 4, 2, 7, 2, 9, 11]
+    print(f'list: {myls}, deduplicated: {remove_duplicates(myls)}')
